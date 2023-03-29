35356b238ea45a9bfd397c73d25bafc5deb825d9
gavillar <gabriel_gomesvillar@hotmail.com>
2023-03-28 22:00:27 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/villardeveloper/greengrocer/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.villardeveloper.greengrocer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/home/home_tab.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+
+class HomeTab extends StatelessWidget {
+   HomeTab({super.key});
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //app bar
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+            children: [
+              TextSpan(
+                text: 'Green',
+                style: TextStyle(
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+              TextSpan(
+                text: 'grocer',
+                style: TextStyle(
+                  color: CustomColors.customConstratColor,
+                ),
+              )
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                backgroundColor: CustomColors.customConstratColor,
+                label: const Text(
+                  '2',
+                  selectionColor: Colors.white,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      // search bar
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+               vertical: 10
+               ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customConstratColor,
+                  size: 21,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      // category
+
+//git
+
+      // grid
+    );
+  }
+}
